e449fc3b1ae23df854dca5b5f2dd8f1ffe9e8cb0
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-09 22:54:59 +0000

    revised assertArraysEqual with proper code.

--- middle.js
+++ middle.js
@@ -1,42 +1,35 @@
-const assertArraysEqual = function(arr1, arr2) {
-  let output = ""
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      output = false
-      break;
-    } 
-  }
-  if (output === false) {
-    console.log(`❌❌❌ The two arrays do NOT match`);
-  } else {
-    console.log(`✅✅✅ The two arrays DO match!`);
-  }
-}
-
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false
-    } 
+      return false;
+    }
   }
-  return true
-}
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ The two arrays DO match!`);
+  } else {
+    console.log(`❌❌❌ The two arrays do NOT match`);
+  }
+};
 
 const middle = function(arr) {
-  let output = []
-  let middle = Math.floor(arr.length/2)
+  let output = [];
+  let middle = Math.floor(arr.length / 2);
   if (arr.length > 2 && arr.length % 2 !== 0) {
-    output.push(arr[middle])
+    output.push(arr[middle]);
   }
   if (arr.length > 2 && arr.length % 2 === 0) {
-    output.push(arr[middle - 1])
-    output.push(arr[middle])
+    output.push(arr[middle - 1]);
+    output.push(arr[middle]);
   }
-  return output
-}
+  return output;
+};
 
-console.log(middle([2, 3]));
-console.log(middle([2, 3, 4]));
-console.log(middle([2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6]))
+assertArraysEqual(middle([2, 3]), []);
+assertArraysEqual(middle([2, 3, 4]), [3]);
+assertArraysEqual(middle([2, 3, 4, 5]), [3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
