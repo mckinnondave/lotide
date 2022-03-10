b51cab65dc0adcf1de1db1c30a92282e9c812578
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-10 00:35:17 +0000

    corrected assertArraysEqual with updated function

--- without.js
+++ without.js
@@ -1,40 +1,32 @@
 const assertArraysEqual = function(arr1, arr2) {
-  let output = ""
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      output = false
-      break;
-    } 
-  }
-  if (output === false) {
-    console.log(`❌❌❌ The two arrays do NOT match`);
-  } else {
+  if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ The two arrays DO match!`);
+  } else {
+    console.log(`❌❌❌ The two arrays do NOT match`);
   }
-}
-
+};
 
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false
-    } 
-  }
-  return true
-}
-
-const without = function(source, itemsToRemove) {
-  let output = []
-  for (let i = 0; i < source.length; i++) {
-    let matchFound = itemsToRemove.includes(source[i])
-    if (matchFound === false) {
-      output.push(source[i])
+      return false;
     }
   }
-  return output
-}
+  return true;
+};
+
+const without = function(source, itemsToRemove) {
+  let output = [];
+  for (let i = 0; i < source.length; i++) {
+    let matchFound = itemsToRemove.includes(source[i]);
+    if (matchFound === false) {
+      output.push(source[i]);
+    }
+  }
+  return output;
+};
 
 const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); 
+console.log(without(words, ["lighthouse"]));
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-console.log(without(["1", "2", "3"], [1, 2, "3"])) 
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
