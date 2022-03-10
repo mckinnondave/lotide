27afc41f493bf0e33dbe4fa8b849c78c3ccb9cf6
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-10 01:18:29 +0000

    function to find if key is in an object

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let arr = Object.keys(object);
+  for (const key of arr) {
+    if (object[key] === value)
+      return key;
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+// findKeyByValue(bestTVShowsByGenre, "The Wire")
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
