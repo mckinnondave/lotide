8c71247b09c6f30c4d8daa1363f660b2e515c43c
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-10 00:30:42 +0000

    fixed syntax errors

--- eqArrays.js
+++ eqArrays.js
@@ -9,12 +9,10 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false
-    } 
+      return false;
+    }
   }
-  return true
-}
-
-
+  return true;
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
