f2fbe9a3f250fcbef384d164c726a37ccb2ce766
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-10 00:32:10 +0000

    fixed syntax errors and made function an expression

--- head.js
+++ head.js
@@ -6,9 +6,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-function head (arr) {
-  return (arr).shift()
-}
+const head = function(arr) {
+  return (arr).shift();
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
