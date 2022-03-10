cca11cf54a337bf0f52a9de8ba2e89aee173da9e
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-10 00:32:57 +0000

    fixed syntax errors

--- tail.js
+++ tail.js
@@ -13,7 +13,7 @@ const tail = function(words) {
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
+console.log((tail(words))); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
 
