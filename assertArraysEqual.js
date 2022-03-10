d47f88e73dc17b99aa8250bcbfdacc0531b0d8e5
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-10 00:27:47 +0000

    fixed syntax errors. removed initial, incorrect code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,36 +1,21 @@
-// const assertArraysEqual = function(arr1, arr2) {
-//   let output = ""
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       output = false
-//       break;
-//     } 
-//   }
-//   if (output === false) {
-//     console.log(`❌❌❌ The two arrays do NOT match`);
-//   } else {
-//     console.log(`✅✅✅ The two arrays DO match!`);
-//   }
-// }
-
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false
-    } 
+      return false;
+    }
   }
-  return true
-}
+  return true;
+};
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ The two arrays DO match!`);
   } else {
-    console.log(`❌❌❌ The two arrays do NOT match`)
+    console.log(`❌❌❌ The two arrays do NOT match`);
   }
-}
+};
 
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual([0, 2, 3], [1, 2, 3])
-assertArraysEqual([1, 2, "3"], [1, 2, 3])
-assertArraysEqual([1, 2, "3"], [1, 2, "3"])
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([0, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, "3"], [1, 2, 3]);
+assertArraysEqual([1, 2, "3"], [1, 2, "3"]);
