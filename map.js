238630b9bb960812d45f0bff756754513f2b4ecc
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-11 00:54:38 +0000

    added one intentionally false test case and adjusted spacing

--- map.js
+++ map.js
@@ -26,17 +26,18 @@ const map = function(array, callback) {
 };
 
 const results1 = map(words, word => word[0]);
-console.log(results1);
 
 const results2 = map(words, word => {
   return word[1];
 });
-const original = [1,2,3,4,5];
-const testCase3 = map(original, num => num % 2 === 0);
 
+const original = [1,2,3,4,5];
+
+const testCase3 = map(original, num => num % 2 === 0);
 
 assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
 assertArraysEqual(results2, ["r", "o", "o", "a", "o"]);
+assertArraysEqual(results2, ["r", "o", "o", "m", "o"]);
 assertArraysEqual(testCase3, [false, true, false, true, false]);
 
 //to confirm original array wasn't changed with testCase3
