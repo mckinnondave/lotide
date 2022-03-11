19feb199872591c43ed90f5fcaee550cfa541834
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-11 02:43:34 +0000

    function with callback to find the first value that matches our callback function

---
+++ findKey.js
@@ -0,0 +1,54 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  let result = "";
+  for (const key in object) {
+    if (callback((object[key]))) {
+      result = key;
+      break;
+    }
+    if (!callback((object[key]))) {
+      result = "undefined";
+    }
+  }
+  return result;
+};
+
+let keyFunc = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+let keyFunc2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5);
+
+let keyFunc3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3);
+
+
+assertEqual(keyFunc, "noma");
+assertEqual(keyFunc2, "undefined");
+assertEqual(keyFunc2, "noma"); // Intentionally false
+assertEqual(keyFunc3, "Akaleri");
