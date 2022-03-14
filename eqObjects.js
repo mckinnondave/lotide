f81fc49b2c1acee92968f602d8efc54fe0d0fc16
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-14 22:33:15 +0000

    exporting function to be used by index file

--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const assertEqual = require("./assertEqual")
+const eqArrays = require("./eqArrays")
 
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
@@ -35,18 +18,15 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
+module.exports = eqObjects
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
 
 // const cd = { c: "1", d: ["2", 3] };
 // const dc = { d: ["2", 3], c: "1" };
-// console.log(eqObjects(cd, dc)); // => true
+// assertEqual(eqObjects(cd, dc), true); // => true
 
 // const cd2 = { c: "1", d: ["2", 3, 4] };
-// console.log(eqObjects(cd, cd2)); // => false
+// assertEqual(eqObjects(cd, cd2), false); // => false
+
+
 
