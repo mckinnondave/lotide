11a0b6183562bd36e18b26f21a953500ed6dc08b
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-14 23:01:22 +0000

    exporting function to be used by index file

--- map.js
+++ map.js
@@ -1,21 +1,9 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays")
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${(arr1)} === ${(arr2)}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${(arr1)} !== ${(arr2)}`);
-  }
-};
+const assertArraysEqual = require("./assertArraysEqual")
 
 const words = ["ground", "control", "to", "major", "tom"];
+
 
 const map = function(array, callback) {
   const results = [];
@@ -25,20 +13,22 @@ const map = function(array, callback) {
   return results;
 };
 
-const results1 = map(words, word => word[0]);
+module.exports = map
 
-const results2 = map(words, word => {
-  return word[1];
-});
+// const results1 = map(words, word => word[0]);
 
-const original = [1,2,3,4,5];
+// const results2 = map(words, word => {
+//   return word[1];
+// });
 
-const testCase3 = map(original, num => num % 2 === 0);
+// const original = [1,2,3,4,5];
 
-assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
-assertArraysEqual(results2, ["r", "o", "o", "a", "o"]);
-assertArraysEqual(results2, ["r", "o", "o", "m", "o"]);
-assertArraysEqual(testCase3, [false, true, false, true, false]);
+// const testCase3 = map(original, num => num % 2 === 0);
 
-//to confirm original array wasn't changed with testCase3
-assertArraysEqual(original, [1,2,3,4,5]);
+// assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+// assertArraysEqual(results2, ["r", "o", "o", "a", "o"]);
+// assertArraysEqual(results2, ["r", "o", "o", "m", "o"]);
+// assertArraysEqual(testCase3, [false, true, false, true, false]);
+
+// //to confirm original array wasn't changed with testCase3
+// assertArraysEqual(original, [1,2,3,4,5]);
