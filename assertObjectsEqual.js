d5dc8ee68293cfcbb1725906f3061e762e43a924
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-14 22:36:46 +0000

    exporting function to be used by index file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,31 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-  let keyArr = Object.keys(object1);
-  for (const key of keyArr) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require("./eqObjects")
 
 const assertObjectsEqual = function(object1, object2) {
   const inspect = require('util').inspect;
@@ -36,9 +9,11 @@ const assertObjectsEqual = function(object1, object2) {
   }
 };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-const cd2 = { c: "1", d: ["2", 3, 4] };
+module.exports = assertObjectsEqual
 
-assertObjectsEqual(cd, dc);
-assertObjectsEqual(cd, cd2);
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+
+// assertObjectsEqual(cd, dc);
+// assertObjectsEqual(cd, cd2);
