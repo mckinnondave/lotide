2e97c90cae27b56afddb2fddf726a0e8d25ac8bf
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-14 16:28:13 +0000

    removed tests in file over to eqArraysTest.js and added export of function

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
-  if(arr1.length !== arr2.length) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
@@ -18,4 +10,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
