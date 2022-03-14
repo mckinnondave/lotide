eaf9277c7312c24740edc3678a2828e4c1602bd8
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-14 16:23:52 +0000

    removed tests in file over to tailTest.js. Added module.exports for function.

--- tail.js
+++ tail.js
@@ -1,19 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(words) {
   let newArr = words.slice(1);
   return newArr;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log((tail(words))); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
+
+
 
 
