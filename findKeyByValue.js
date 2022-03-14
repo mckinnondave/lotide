7d36c29863dc170ca5a23b9bce1fc39dcb3ba6cb
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-14 22:51:13 +0000

    exporting function to be used by index file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = require("./assertEqual")
 
 const findKeyByValue = function(object, value) {
   let arr = Object.keys(object);
@@ -14,14 +8,16 @@ const findKeyByValue = function(object, value) {
   }
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
+module.exports = findKeyByValue;
 
-// findKeyByValue(bestTVShowsByGenre, "The Wire")
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+// // findKeyByValue(bestTVShowsByGenre, "The Wire")
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
