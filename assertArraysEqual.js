31e56766dae24f42bc4db2d6129424c6a6d94d28
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-14 16:36:27 +0000

    removed tests and moved them to assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -15,7 +8,5 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([0, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, "3"], [1, 2, 3]);
-assertArraysEqual([1, 2, "3"], [1, 2, "3"]);
+module.exports = assertArraysEqual;
+
