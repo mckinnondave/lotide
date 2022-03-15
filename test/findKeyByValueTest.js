069beba114c31aad8346c6ed1236b13856f69ec5
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-15 03:24:09 +0000

    test code for findKeyByValue.js

---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,24 @@
+const assert = require('chai').assert;
+const findKeyByValue = require("../findKeyByValue");
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+describe("#findKeyByValue", () => {
+
+  it("returns 'drama' for 'The Wire", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+
+  it("returns 'undefined' for 'The Expanse", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  });
+
+  it("returns 'sci_fi' for 'The Expanse", () => {
+    assert.strictEqual(findKeyByValue(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi"));
+  });
+
+});
